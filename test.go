981094d90de0fb36a74528543df6e07d7cedbb2e
jp <jp@txt3.com>
2022-06-27 16:52:22 +0100

    parse contract call data

--- test.go
+++ test.go
@@ -86,7 +86,7 @@ func main() {
 
 	enc1 := crypto.Keccak256([]byte("submitPriceHashes(uint256,uint256[],bytes32[])"))
 
-	hStr := hexutil.Encode(enc1)[2:8]
+	hStr := hexutil.Encode(enc1)[1:7]
 
 	log.Println("AAA:", hStr)
 
